ccc5328903610e7c6a4410d273294d0b632b154f
talgiladi@gmail.com <talgiladi@gmail.com>
2024-09-09 14:47:01 +0300

    first commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<QueueConsumerService>();
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
+
+public class QueueConsumerService : BackgroundService
+{
+    private readonly IModel _channel;
+
+    public QueueConsumerService()
+    {
+        var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+        var connection = factory.CreateConnection();
+        _channel = connection.CreateModel();
+        _channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        };
+        _channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        _channel.Close();
+        base.Dispose();
+    }
+}
+
---
+++ Send/Send.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory { HostName = "rabbitmq" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+int index = 0;
+while(true){
+    string message = $"#{index++} {DateTime.UtcNow}";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty,
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+    Console.WriteLine($" [x] Sent {message}");
+    System.Threading.Thread.Sleep(1000);
+}
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
